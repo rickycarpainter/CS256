1e6634074712bf10a8b71700d176de9abfb57ef7
Joseph <josephmhansen@gmail.com>
2015-03-30 09:36:52 -0600

    updated loading data and showing answers

--- js/script.js
+++ js/script.js
@@ -1,69 +1,96 @@
-function initializeVariables(){
+function initializeVariables()
+{
 	this.currentSelectedQuestion = -1;
 	var m = new model();
 	loadModelDummyData(m);
 }
 
-function setQuestion(newID){
-	//alert('setting new question');
-	this.currentSelectedQuestion = newID;
-	//alert('currentlySelectedQuestion is '+currentSelectedQuestion);
-	reloadData();
-}
+function initialDataLoad()
+{
+    console.log("in init data load");
 
-function foo(){
-	alert('Foo');
-}
-
-function reloadData(){
 	var questions = m.getQuestions("");
-	var questionList = document.getElementById('questions');
-	while (questionList.firstChild) {
+	var questionList = document.getElementsByClassName('question-list')[0];
+	while (questionList.firstChild)
+    {
 		questionList.removeChild(questionList.firstChild);
 	}
-	for(var i = 0; i < questions.length; i++){
+	for(var i = 0; i < questions.length; i++)
+    {
 		var currentQuestion = questions[i];
 		var currentQuestionID = currentQuestion.ID;
 		var questionDiv = document.createElement('div');
 		questionDiv.className = "question";
+        questionDiv.id = currentQuestionID;
+
+        questionDiv.onclick = function(event) 
+            { 
+                var questionEl = event.target;
+                while (questionEl.className !== "question")
+                {
+                    if (questionEl.tagName === "article")
+                        return;
+                    questionEl = questionEl.parentNode;
+                }
+                openQuestion(questionEl)
+            };
+
 		var questionHTML = "";
 		questionHTML += "<div class='vote'>\n";
-		questionHTML += "<button type='button' onclick='setQuestion("+currentQuestion.ID+")' class='upvote'></button>\n";
+        // TODO fix voting
+		//questionHTML += "<button type='button' onclick='setQuestion("+currentQuestion.ID+")' class='upvote'></button>\n";
+		questionHTML += "<button type='button' class='upvote'></button>\n";
 		questionHTML += "</div>\n";
 		questionHTML += "<div class='score'>"+currentQuestion.upvotes+"</div>\n";
-		questionHTML += "<div class='text' onclick='setQuestion("+currentQuestionID+")'>"+currentQuestion.title+"</div>\n";
+		questionHTML += "<div class='text'>"+currentQuestion.title+"</div>\n";
 		questionHTML += "<div class='answers'>"+m.getQuestionAnswers(currentQuestion.ID).length+"<div>answers</div></div>\n";
 		questionDiv.innerHTML =  questionHTML;
+
 		questionList.appendChild(questionDiv);
-		if(currentQuestion.ID == currentSelectedQuestion){
-			//alert(currentQuestion.ID + ' = '+currentSelectedQuestion);
-			var answerListDiv = document.createElement('div');
-			answerListDiv.className = "answer-list";
-			var answers = m.getQuestionAnswers(currentQuestionID);
-			answerListDiv.innerHTML = '';
-			for(var j = 0; j < answers.length; j++){
-				var currentAnswer = answers[j];
-				var answerHTML = "<div class='answer'>\n";
-				answerHTML += "<div class='vote'>\n";
-				answerHTML += "<button class='upvote'></button>\n";
-				answerHTML += "<button class='downvote'></button>\n";
-				answerHTML += "</div>\n";
-				var currentScore = currentAnswer.upvotes - currentAnswer.downvotes;
-				answerHTML += "<div class='score'>"+currentScore+"</div>\n";
-				answerHTML += "<div class='text'>"+currentAnswer.content+"</div>\n";
-				answerHTML += "</div>\n";
-				answerListDiv.innerHTML += answerHTML;
-			}
-			questionList.appendChild(answerListDiv);
-		} else {
-			//alert('Not loading answers');
-		}
+
+        console.log("added a new question to questionList");
 	}
-	//alert('reloading terminated');
 }
 
+function openQuestion(questionEl)
+{
+    console.log("in openQuestion");
+
+    var questionId = questionEl.id;
+
+    var articleElement = document.getElementsByTagName("article")[0];
+    while (document.getElementsByClassName("answer-list").length > 0)
+    {
+        var elToDelete = document.getElementsByClassName("answer-list")[0];
+        elToDelete.parentNode.removeChild(elToDelete);
+    }
+
+	var answerListDiv = document.createElement('div');
+	answerListDiv.className = "answer-list";
+	var answers = m.getQuestionAnswers(questionId);
+	answerListDiv.innerHTML = '';
+	for(var j = 0; j < answers.length; j++)
+    {
+		var currentAnswer = answers[j];
+		var answerHTML = "<div class='answer'>\n";
+		answerHTML += "<div class='vote'>\n";
+		answerHTML += "<button class='upvote'></button>\n";
+		answerHTML += "<button class='downvote'></button>\n";
+		answerHTML += "</div>\n";
+		var currentScore = currentAnswer.upvotes - currentAnswer.downvotes;
+		answerHTML += "<div class='score'>"+currentScore+"</div>\n";
+		answerHTML += "<div class='text'>"+currentAnswer.content+"</div>\n";
+		answerHTML += "</div>\n";
+		answerListDiv.innerHTML += answerHTML;
+	}
+    questionEl.insertAdjacentHTML('afterend', answerListDiv.outerHTML);
+
+    console.log("added new answerList to questionList");
+}
+
+
 window.addEventListener("DOMContentLoaded", function() {
-	//alert('DOM loaded');
+	console.log("DOM loaded");
 	initializeVariables();
-	reloadData();
+    initialDataLoad();
 }, false);
